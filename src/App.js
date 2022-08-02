7a2139e646e30fb6fb4f57589f96d9a65dd9f85a
kerolesnabill <kerolesnabil513@gmail.com>
2022-08-02 09:27:09 +0200

    Create Home page and AnimatedLetters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo.png';
+import './Home.scss';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import Logo from './Logo/Logo';
+
+const Home = () => {
+  const [lettersClass, setLettersClass] = useState('text-animate');
+  const nameArray = ['e', 'r', 'o', 'l', 'e', 's'];
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLettersClass('text-animate-hover');
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={lettersClass}>H</span>
+          <span className={`${lettersClass} _12`}>i,</span>
+          <br />
+          <span className={`${lettersClass} _13`}>I</span>
+          <span className={`${lettersClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="" />
+          <AnimatedLetters
+            lettersClass={lettersClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            lettersClass={lettersClass}
+            strArray={jobArray}
+            idx={21}
+          />
+        </h1>
+        <h2>Forntend Developer / React js Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Home;
