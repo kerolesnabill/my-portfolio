010e143bae985d9c02da8fd334a7f80447fe3bf6
kerolesnabill <kerolesnabil513@gmail.com>
2022-08-05 18:38:52 +0200

    Add some projects

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Work.scss';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import ProjectCard from './ProjectCard/ProjectCard';
+import vrImg from '../../assets/images/vr.png';
+import travelImg from '../../assets/images/travel.png';
+import notesImg from '../../assets/images/notes.png';
+import foodImg from '../../assets/images/food.png';
 
 const Work = () => {
   const [lettersClass, setLettersClass] = useState('text-animate');
@@ -26,7 +31,33 @@ const Work = () => {
           </h1>
         </div>
       </div>
-      <div className="work"></div>
+      <div className="work">
+        <ProjectCard
+          title="Food Order"
+          description="Website to order food"
+          technologies="React, Redux, Tailwind css, Firebase"
+          img={foodImg}
+        />
+        <ProjectCard
+          title="Travel Website"
+          description="Website for traveling"
+          technologies="React, Bootstrap"
+          img={travelImg}
+        />
+
+        <ProjectCard
+          title="Notes Website"
+          description="Website like google keep"
+          technologies="React, Sass, Firebase"
+          img={notesImg}
+        />
+        <ProjectCard
+          title="VR Website"
+          description="Landing page - VR website"
+          technologies="React, Tailwind css"
+          img={vrImg}
+        />
+      </div>
     </>
   );
 };
